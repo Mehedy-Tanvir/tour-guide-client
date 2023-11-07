e60d8fda271bcf7cfdc920b3fb9b6264ccef542e
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-11-07 10:24:34 +0600

    error page ui improved

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -6,16 +6,15 @@ const ErrorPage = () => {
   return (
     <>
       <Navbar></Navbar>
-      <div className="flex flex-col items-center justify-center h-screen gap-6">
+      <div className="flex flex-col items-center justify-center mx-auto max-w-[1400px] pb-10 min-h-screen gap-6">
         <div className="flex items-center justify-center">
-          <img className="" src="/logo.svg" alt="logo" />
-          <p className="text-4xl font-semibold">Travello</p>
+          <img className="max-h-[400px]" src="/404-error.gif" alt="image" />
         </div>
         <h1 className="text-4xl font-bold text-center text-[#0B0B0BB3]">
           Error 404! Page not found!
         </h1>
         <Link to="/">
-          <button className="bg-[#FA7436] hover:opacity-90 text-white text-2xl font-semibold h-[60px] px-[20px] rounded-none">
+          <button className="bg-[#FA7436] hover:opacity-90 text-white text-2xl font-semibold h-[60px] px-[20px] rounded-lg">
             Go Home
           </button>
         </Link>
