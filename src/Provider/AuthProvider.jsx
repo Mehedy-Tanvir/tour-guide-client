dd33ea6e32422a8e0e902bb2cde420de9f318545
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-11-07 20:13:18 +0600

    jwt token in cookies set up functionality added

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -10,6 +10,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import auth from "../firebase/firebaseConfig";
+import axios from "axios";
 export const AuthContext = createContext(null);
 const googleProvider = new GoogleAuthProvider();
 const AuthProvider = ({ children }) => {
@@ -45,13 +46,27 @@ const AuthProvider = ({ children }) => {
   };
   //   observe user
   useEffect(() => {
-    const unSubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setUser(user);
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      const userEmail = currentUser?.email || user?.email;
+      const loggedUser = { email: userEmail };
+      if (currentUser) {
+        setUser(currentUser);
         setLoading(false);
+        axios
+          .post("http://localhost:3000/jwt", loggedUser, {
+            withCredentials: true,
+          })
+          .then((res) => console.log(res))
+          .catch((err) => console.log(err));
       } else {
         setUser(null);
         setLoading(false);
+        axios
+          .post("http://localhost:3000/logout", loggedUser, {
+            withCredentials: true,
+          })
+          .then((res) => console.log(res))
+          .catch((err) => console.log(err));
       }
     });
     return () => {
