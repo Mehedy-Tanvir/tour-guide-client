a7f1c0e15b1656445c8e929807b566587248d945
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-12-12 18:42:17 +0600

    subscription functionality added

--- src/Components/Subscribe/Subscribe.jsx
+++ src/Components/Subscribe/Subscribe.jsx
@@ -1,4 +1,41 @@
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
 const Subscribe = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubscribe = async () => {
+    if (!validateEmail(email)) {
+      toast.error("Please enter a valid email address.");
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        "https://tour-guide-server-alpha.vercel.app/subscribe",
+        { email }
+      );
+
+      if (response.status === 200) {
+        toast.success(
+          "Subscription successful! You will receive exclusive deals and offers."
+        );
+      } else {
+        toast.error("Subscription failed. Please try again later.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+
+      toast.error("An error occurred. Please try again later.");
+    }
+  };
+
+  const validateEmail = (email) => {
+    // You can implement a more sophisticated email validation logic here
+    return /\S+@\S+\.\S+/.test(email);
+  };
+
   return (
     <div className="max-w-[1400px] mx-auto min-h-[450px] bg-[#FA7436] mb-[40px] rounded-[16px] drop-shadow-lg relative">
       <img className="absolute top-4 right-4" src="/Element.svg" alt="" />
@@ -12,8 +49,13 @@ const Subscribe = () => {
           <input
             className="input input-bordered md:join-item"
             placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
-          <button className="rounded-r-xl bg-[#FA7436] border-2 normal-case text-[16px] hover:text-[#FA7436] border-white text-white btn md:join-item">
+          <button
+            onClick={handleSubscribe}
+            className="rounded-r-xl bg-[#FA7436] border-2 normal-case text-[16px] hover:text-[#FA7436] border-white text-white btn md:join-item"
+          >
             Subscribe
           </button>
         </div>
